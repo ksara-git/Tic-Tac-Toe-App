a8c0aa2415965a23c51c52256fc3121332c78898
ksara-git <sarakhan8047@gmail.com>
2026-04-21 13:10:41 +0530

    UC1: Display empty Tic Tac Toe board

---
+++ TicTacToeUC1.java
@@ -0,0 +1,23 @@
+public class TicTacToeUC1 {
+
+    public static void main(String[] args) {
+
+        //3x3 board
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        //Printing the board
+        System.out.println("Tic-Tac-Toe Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println(); // move to next row
+        }
+    }
+}
